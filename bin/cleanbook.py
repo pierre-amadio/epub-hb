3f9824f605390f901a30926cd4881f1f7e0e3fd2
Pierre Amadio <pierre.amadio@laposte.net>
2022-05-07 20:57:01 +0200

    how to deal with qetiv/kere?

--- bin/cleanbook.py
+++ bin/cleanbook.py
@@ -17,10 +17,48 @@ print("Extracting book from %s"%shortName)
 
 def createChapterHtml(chapter):
     chapterId=chapter["osisID"]
-    print(chapterId)
+    
+    data={}
+
+    m=re.match("(\S+)\.(\d+)",chapterId)
+    if not m:
+        print("Cannot parse chapter",chapterId)
+        sys.exit()
+    bookId=m.group(1)
+    bookName=bookAbbr[bookId]
+    chapterNbr=m.group(2)
+    data["id"]=chapterNbr
+    data["nbr"]=chapterNbr
+
+    data["verses"]=[]
     for verse in chapter.find_all("verse"):
+        curVerse={}
         verseId=verse["osisID"]
-        print(verseId)
+        m=re.match("(\S+)\.(\d+)",verseId)
+        if not m:
+            print("Cannot parse verse",verseId)
+            sys.exit()
+        verseNbr=m.group(2)
+        curVerse["nbr"]=verseNbr
+        curVerse["content"]=""        
+        for node in verse.find_all():
+            if(node.name=="w"):
+                word=node.string
+                if(word.find("/")>-1):
+                    word=word.replace("/","")
+                curVerse["content"]+=word
+            elif(node.name=="seg"):
+                tmp="<span class='%s'>%s</span>"%(node["type"],node.string)
+                curVerse["content"]+=tmp
+            else:
+                print(verseId)
+                print(node)
+
+
+        data["verses"].append(curVerse)
+
+       
+"""
         for node in verse.find_all():
             #print("name=",node.name)
             if node.name!="w":
@@ -28,17 +66,13 @@ def createChapterHtml(chapter):
                 print(node)
             if "type" in node.attrs:
                 print("type)",node["type"])
-
+"""
 
 
 with open(inputFile) as fp:
   soup=BeautifulSoup(fp, features='xml')
   for book in soup.find_all('div',type="book"):
-    bookId=book["osisID"]
-    print(bookId)
-    if not bookId in bookAbbr:
-        print("AIE")
-    #for chapter in book.find_all("chapter"):
-    #   createChapterHtml(chapter)
+    for chapter in book.find_all("chapter"):
+       createChapterHtml(chapter)
 
 
